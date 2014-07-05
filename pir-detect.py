19b889168042861245fbde98f757222dd83e416f
Kevin K <visualkev@visualkev.com>
2014-07-05 16:59:56 -0700

    change to event_detect to monitor both gpio 3 and 7

--- pir-detect.py
+++ pir-detect.py
@@ -11,15 +11,26 @@ gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False)
 
 gpio.setup(3, gpio.IN)
+gpio.setup(7, gpio.IN)
 
 retcode = subprocess.call(["/home/pi/cam.sh"])
 
+gpo.add_event_detect(3, gpio.RISING)
+gpo.add_event_detect(7, gpio.RISING)
+
+
 while True:
-	gpio.wait_for_edge(3, gpio.RISING)
-	print 'motion!'
-	#process = subprocess.Popen(["/usr/bin/raspistill", "-n", "-t", "60000", "--timelapse", "1000", "-o", "/home/pi/camera/img%04d.jpg", "-l", "/home/pi/camera/latest.jpg", "-w", "640", "-h", "480", "-q", "20"], stdout=subprocess.PIPE).communicate()[0]	
-	process = subprocess.Popen(['/home/pi/startcam.sh'], stdout=subprocess.PIPE).communicate()[0]
-	print process
+	#gpio.wait_for_edge(3, gpio.RISING)
+	if gpio.event_detected(3):
+		print 'motion!'
+		#process = subprocess.Popen(["/usr/bin/raspistill", "-n", "-t", "60000", "--timelapse", "1000", "-o", "/home/pi/camera/img%04d.jpg", "-l", "/home/pi/camera/latest.jpg", "-w", "640", "-h", "480", "-q", "20"], stdout=subprocess.PIPE).communicate()[0]	
+		process = subprocess.Popen(['/home/pi/startcam.sh'], stdout=subprocess.PIPE).communicate()[0]
+		#print process
+	
+	if gpio.event_detected(7):
+		gpio.cleanup()
+		process = subprocess.Popen(['/home/pi/lbshutdown.sh'], stdout=subprocess.PIPE).communicate()[0]
+	
 
 gpio.cleanup()
 
